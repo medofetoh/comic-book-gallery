d966bbb2a7aa6ad13bbdc3b80bf67caa5a5ed268
Mohamed Hassan <engmedofetoh2016@gmail.com>
2020-06-09 02:46:39 -0400

    create the project

---
+++ src/comic-book-Gallary/comic-book-Gallary/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace comic_book_Gallary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
